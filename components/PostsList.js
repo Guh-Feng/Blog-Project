99816c246a04d00eda9414a8da39dfb1bf93e9b7
Guh-Feng <58538080+Guh-Feng@users.noreply.github.com>
2023-08-13 00:34:09 -0400

    Text Decorations, Completed Blog Posts List HTML

--- components/PostsList.js
+++ components/PostsList.js
@@ -15,8 +15,8 @@ export default function PostsList(){
                 <div className={`${styles.title} ${roboto.className}`}>Blog Posts</div>
                 <div className={`${styles.year} ${roboto.className}`}>2023</div>
                 <div className={`${styles.month} ${roboto.className}`}>August</div>
-                <Link href='/posts/blog_post_on_posting_blogs' className={`${styles.postLinks} ${roboto.className}`}>Blog Post on Posting Blogs</Link>
-                <Link href='/posts/the_fields_of_computer_science' className={`${styles.postLinks} ${roboto.className}`}>Thoughts on Computer Science</Link>
+                <Link href='/posts/blog_post_on_posting_blogs' className={`${styles.postLinks} ${roboto.className}`} style={{textDecoration : 'none'}}>Blog Post on Posting Blogs</Link>
+                <Link href='/posts/the_fields_of_computer_science' className={`${styles.postLinks} ${roboto.className}`} style={{textDecoration : 'none'}}>Thoughts on Computer Science</Link>
             </div>
         </>
     )
